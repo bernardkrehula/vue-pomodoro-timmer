41937a66a93df5ad44f2ff816dba9ca6c0cb72ea
Bernard Krehula <krehula.bernard456@gmail.com>
2026-09-13 13:18:30 +0200

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,11 +1,5 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import './main.css'
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+createApp(App).mount('#app')
