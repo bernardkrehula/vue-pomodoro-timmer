834d207e1caf6c6fb06dbcb9d041b3d4130c0427
Bernard Krehula <krehula.bernard456@gmail.com>
2026-09-13 13:42:13 +0200

    feat: add <PomodoroTimmer> component

---
+++ src/components/PomodoroTimmer.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import Btn from "./Btn.vue";
+
+const startTimmer = () => {};
+
+const restartTimmer = () => {};
+</script>
+
+<template>
+  <div class="timmer">
+    <h1 class="title">Pomodoro Rd 1</h1>
+    <div class="time">
+      <span>25 : 00</span>
+    </div>
+    <div class="btns">
+      <Btn variation="primary" @click="">Start ▶︎</Btn>
+      <Btn variation="primary" @click="">Restart ⟳</Btn>
+    </div>
+  </div>
+</template>
+
+<style>
+.timmer {
+  display: flex;
+  flex-direction: column;
+  gap: 60px;
+  width: fit-content;
+  margin: 100px auto;
+  justify-content: center;
+}
+.title {
+  font-size: 6rem;
+  color: white;
+  text-transform: uppercase;
+  font-family: Arial, Helvetica, sans-serif;
+}
+.time {
+  font-family: Arial, Helvetica, sans-serif;
+  color: white;
+  font-size: 17rem;
+  font-weight: 700;
+}
+
+.btns {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  margin: 0 auto;
+}
+</style>
