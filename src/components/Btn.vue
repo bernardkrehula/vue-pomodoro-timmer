d872cb6e66ec550eca840c3f697389c6480a30c3
Bernard Krehula <krehula.bernard456@gmail.com>
2026-09-13 13:42:22 +0200

    feat: add <Btn> componnent

---
+++ src/components/Btn.vue
@@ -0,0 +1,29 @@
+<script>
+ export default {
+    props: {
+        /* click: () => void, */
+        variation: String
+    }
+ }
+</script>
+
+<template>
+  <button :class="['btn', `btn-${variation}`]" @click="click"><slot></slot></button>
+</template>
+
+<style>
+.btn{
+    background-color: red;
+    border: none;
+    color: white;
+    padding: 5px 20px;
+    font-size: 2.3rem;
+    height: 50px;
+    border-radius: 8px;
+    cursor: pointer;
+    transition: 0.2s all;
+}
+.btn:hover{
+    background-color: rgb(194, 11, 11);
+}
+</style>
